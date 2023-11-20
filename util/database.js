b562f2d0f92e0f808c6d00c836e0e01f5a5948f0
zainhasan <zain.hasan@kravemart.com>
2023-11-21 00:38:20 +0500

    sql sync(migration) + fetch products

--- util/database.js
+++ util/database.js
@@ -1,8 +1,9 @@
-const Sequelize = require('sequelize');
+const Sequelize = require("sequelize");
 
-const sequelize = new Sequelize('node-complete', 'root', 'nodecomplete', {
-  dialect: 'mysql',
-  host: 'localhost'
+const sequelize = new Sequelize("node-complete", "root", "mysql123", {
+  dialect: "mysql",
+  host: "localhost",
+  // port: "3000",
 });
 
 module.exports = sequelize;
